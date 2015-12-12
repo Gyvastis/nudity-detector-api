2f338583d1181a46c02fc2fbe8afc531404e1613
Vaidas <shibalba1@gmail.com>
2015-12-12 19:41:15 +0200

    api works

--- index.php
+++ index.php
@@ -6,17 +6,56 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-use Psr\Http\Message\ServerRequestInterface;
-use Psr\Http\Message\ResponseInterface;
-
 $app = new Slim\App();
 
-$app->post('/check', function(ServerRequestInterface $request, ResponseInterface $response, $args){
+$app->post('/check', function(Slim\Http\Request $request, Slim\Http\Response $response){
     if($request->getContentType() == 'application/json') {
-        $response->write(json_encode([
-            'success' => true
-        ]));
-        return $response->withHeader(
+        $request_params = $request->getParsedBody();
+        $response_params = [
+            'success' => false,
+            'message' => '',
+            'data' => []
+        ];
+
+        if(empty($request_params)){
+            $response_params['message'] = 'No parameters supplied';
+
+        }
+        else if(! isset($request_params['url']) || empty($request_params['url'])){
+            $response_params['message'] = 'Image URL parameter missing';
+        }
+        else{
+            $source_url = 'http://www.nudedetect.com/';
+
+            $curl = new \Curl\Curl();
+            $curl->setReferrer($source_url);
+            $curl->post($source_url . 'process.php', [
+                'url' => $request_params['url']
+            ]);
+
+            if ($curl->error) {
+                $response_params['message'] = $curl->error_code;
+            }
+            else {
+                $image_check_response = strtolower($curl->response);
+                preg_match_all("/([0-9]+\.[0-9]+%)<\/h[0-9]>\s([a-z]+)/i", $image_check_response, $image_check_data, PREG_PATTERN_ORDER);
+
+                if(empty($image_check_data)){
+                    $request_params['message'] = 'Image check failed';
+                }
+                else{
+                    $response_params['data'] = [
+                        'nude' => $image_check_data[1][0],
+                        'minimal' => $image_check_data[1][1]
+                    ];
+                    $response_params['success'] = true;
+                }
+
+            }
+            $curl->close();
+        }
+
+        return $response->write(json_encode($response_params))->withHeader(
             'Content-Type',
             'application/json'
         );
